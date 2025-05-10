3b60568a029284037c5ef33e5827ec3229caa580
saman.mohammadi <autosam.sm@gmail.com>
2025-05-10 13:08:30 +0330

    feat: script copy posts.json to public after build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vitejs.dev/config/vite
 export default defineConfig({
   plugins: [react()],
-  base: '/blog/'
+  base: '/blog/',
+  build: {
+    rollupOptions: {
+      plugins: [
+        {
+          name: 'move-json',
+          buildEnd() {
+            const srcPath = path.resolve(__dirname, 'assets/json/posts.json');
+            const destPath = path.resolve(__dirname, 'public/posts.json');
+            fs.copyFileSync(srcPath, destPath);
+          },
+        },
+      ],
+    },
+  },
 })
